6a026249c371e6c1947ef04d4891507f08e2df29
Cyril Leroux <cyril.leroux@cgi.com>
2013-06-19 16:27:38 +0200

    Added sayTime() functionality

--- src/org/es/butler/MainActivity.java
+++ src/org/es/butler/MainActivity.java
@@ -15,82 +15,86 @@ import org.es.api.AgendaApi;
 import org.es.api.WeatherApi;
 import org.es.api.factory.AgendaApiFactory;
 import org.es.api.factory.WeatherApiFactory;
-import org.es.butler.utils.TimeUtils;
+import org.es.butler.logic.TimeLogic;
 
 import java.util.Locale;
-
-import corg.es.butler.R;
 
 /**
  * Created by Cyril Leroux on 17/06/13.
  */
 public class MainActivity extends Activity implements OnInitListener, OnClickListener {
 
-	private static final String TAG = "ButlerActivity";
-	private TextToSpeech mTTS;
+    private static final String TAG = "ButlerActivity";
+    private TextToSpeech mTTS;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		mTTS = new TextToSpeech(getApplicationContext(), this);
+        mTTS = new TextToSpeech(getApplicationContext(), this);
 
-		((Button) findViewById(R.id.btn_daily_speech)).setOnClickListener(this);
-	}
+        ((Button) findViewById(R.id.btn_daily_speech)).setOnClickListener(this);
+    }
 
-	@Override
-	protected void onDestroy() {
-		if (mTTS != null) {
-			mTTS.stop();
-			mTTS.shutdown();
-			mTTS = null;
-		}
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        if (mTTS != null) {
+            mTTS.stop();
+            mTTS.shutdown();
+            mTTS = null;
+        }
+        super.onDestroy();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public void onInit(int status) {
-		if (status != TextToSpeech.SUCCESS) {
-			Log.e(TAG, "TTS initialization failed");
-		}
-		int result = mTTS.setLanguage(Locale.US);
+    @Override
+    public void onInit(int status) {
+        if (status != TextToSpeech.SUCCESS) {
+            Log.e(TAG, "TTS initialization failed");
+            return;
+        }
 
-		if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
-			Log.e(TAG, "This Language is not supported");
-		}
-	}
+        int result = TextToSpeech.LANG_MISSING_DATA;
+        if (mTTS.isLanguageAvailable(Locale.UK) == TextToSpeech.LANG_AVAILABLE) {
+            result = mTTS.setLanguage(Locale.UK);
+        } else {
+            result = mTTS.setLanguage(Locale.US);
+        }
 
-	@Override
-	public void onClick(View v) {
+        if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+            Log.e(TAG, "This Language is not supported");
+        }
+    }
 
-		switch(v.getId()) {
-		case R.id.btn_daily_speech :
-			dailySpeech(true);
-			break;
+    @Override
+    public void onClick(View v) {
 
-		default:
-			break;
-		}
-	}
+        switch (v.getId()) {
+            case R.id.btn_daily_speech:
+                dailySpeech(true);
+                break;
 
-	/**
-	 * Say the daily speech
-	 * @param force Forces the speech even if the {@link #cancelDailySpeech()} trigger returns true.
-	 */
-	private void dailySpeech(boolean force) {
-		if (cancelDailySpeech() && !force) {
-			return;
-		}
+            default:
+                break;
+        }
+    }
 
-		sayHello();
+    /**
+     * Say the daily speech
+     *
+     * @param force Forces the speech even if the {@link #cancelDailySpeech()} trigger returns true.
+     */
+    private void dailySpeech(boolean force) {
+        if (cancelDailySpeech() && !force) {
+            return;
+        }
 
         WeatherApi weather = WeatherApiFactory.getWeatherAPI();
         weather.checkWeather();
@@ -99,36 +103,64 @@ public class MainActivity extends Activity implements OnInitListener, OnClickLis
         agenda.checkTodayEvents();
         agenda.checkUpcomingEvent();
 
-	}
+        Time now = new Time();
+        now.setToNow();
 
-	private boolean cancelDailySpeech() {
-		// TODO implement the conditions to cancel daily speech
-		return false;
-	}
+        TimeLogic time = new TimeLogic(now);
+        sayHello(time);
+        sayTime(time);
+
+        mTTS.speak("You have no appointment today.", TextToSpeech.QUEUE_ADD, null);
+    }
+
+    private boolean cancelDailySpeech() {
+        // TODO implement the conditions to cancel daily speech
+        return false;
+    }
 
     /**
      * Say hello out loud.
      * The spoken text depends on the time of day.
+     *
+     * @param time The time defining the spoken text.
      */
-	private void sayHello() {
-		Time now	= new Time();
-		now.setToNow();
+    private void sayHello(final TimeLogic time) {
 
-		String text = null;
+        String text = null;
 
-		if (TimeUtils.isMorning(now)) {
-			text = getString(R.string.good_morning);
+        if (time.isMorning()) {
+            text = getString(R.string.good_morning);
 
-		} else if (TimeUtils.isAfternoon(now)) {
-			text = getString(R.string.good_afternoon);
+        } else if (time.isAfternoon()) {
+            text = getString(R.string.good_afternoon);
 
-		} else if (TimeUtils.isEvening(now)) {
-			text = getString(R.string.good_evening);
+        } else if (time.isEvening()) {
+            text = getString(R.string.good_evening);
 
-		} else if (TimeUtils.isNight(now)) {
-			text = getString(R.string.good_night);
-		}
+        } else if (time.isNight()) {
+            text = getString(R.string.good_night);
 
-		mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
-	}
+        } else {
+           Log.e(TAG, "Unexpected Time value : " + time.getTime().format("HH:mm:ss"));
+        }
+
+        mTTS.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+    /**
+     * Say the time out loud.
+     *
+     * @param time the time to speak out loud.
+     */
+    private void sayTime(final TimeLogic time) {
+        mTTS.setSpeechRate(0.9f);
+        final String text = time.getPronunciation();
+        if (text == null || text.isEmpty()) {
+            Log.e(TAG, "sayTime(), couldn't get pronunciation.");
+        }
+
+        mTTS.speak(text, TextToSpeech.QUEUE_ADD, null);
+
+        mTTS.setSpeechRate(1f);
+    }
 }
---
+++ src/org/es/butler/logic/TimeLogic.java
@@ -0,0 +1,126 @@
+package org.es.butler.logic;
+
+import android.text.format.Time;
+
+/**
+ * Created by Cyril Leroux on 18/06/13.
+ * <p/>
+ * This class defines all the needed logic concerning the time passed to the constructor.
+ */
+public class TimeLogic {
+
+    private Time mTime;
+    private Time mMorning;
+    private Time mMidday;
+    private Time mEvening;
+    private Time mNight;
+
+    public TimeLogic(final Time time) {
+        mTime = time;
+        mMorning = new Time(time);
+        mMidday = new Time(time);
+        mEvening = new Time(time);
+        mNight = new Time(time);
+
+        mMorning.set(0, 0, 5, time.monthDay, time.month, time.year);
+        mMidday.set(0, 0, 12, time.monthDay, time.month, time.year);
+        mEvening.set(0, 0, 18, time.monthDay, time.month, time.year);
+        mNight.set(0, 0, 23, time.monthDay, time.month, time.year);
+    }
+
+    public Time getTime() {
+        return mTime;
+    }
+
+    /**
+     * @return True if time is between 05:00 -> 11:59
+     */
+    public boolean isMorning() {
+        return mTime.equals(mMorning) || mTime.after(mMorning) && mTime.before(mMidday);
+    }
+
+    /**
+     * @return True if time is between 12:00 -> 17:59
+     */
+    public boolean isAfternoon() {
+        return mTime.equals(mMidday) || mTime.after(mMidday) && mTime.before(mEvening);
+    }
+
+    /**
+     * @return True if time is between 18:00 -> 22:59
+     */
+    public boolean isEvening() {
+        return mTime.equals(mEvening) || mTime.after(mEvening) && mTime.before(mNight);
+    }
+
+    /**
+     * @return True if time is between 0:00 -> 04:59 or 23:00 -> 0:00
+     */
+    public boolean isNight() {
+        return mTime.before(mMorning) || mTime.equals(mNight) || mTime.after(mNight);
+    }
+
+    public String getPronunciation() {
+        return getPronunciationEn(mTime);
+    }
+
+    private String getPronunciationEn(final Time time) {
+        final int minute  = time.minute;
+        int hour = get12hFormat(time.hour);
+        int nextHour = get12hFormat(getNextHour(time.hour));
+
+        final StringBuilder sb = new StringBuilder();
+        sb.append("Time is now ");
+        sb.append(get12hFormat(time.hour)).append(" ");
+
+        if (time.minute > 0 && time.minute < 10) {
+            sb.append("o").append(time.minute);
+        } else if (time.minute >= 10) {
+            sb.append(time.minute);
+        }
+
+        sb.append(getSuffix(time.hour));
+
+        return sb.toString();
+//        if (minute == 0) {
+//            sb.append(hour).append(" o'clock");
+//
+//        } else if (minute == 15) {
+//            sb.append("a quarter past ").append(hour);
+//
+//        } else if (minute == 30) {
+//            sb.append("half past ").append(hour);
+//
+//        } else if (minute == 45) {
+//            sb.append("a quarter to ").append(nextHour);
+//
+//        } else if (minute < 30) {
+//            sb.append(minute).append(" past ").append(hour);
+//
+//        } else if (minute > 30) {
+//            sb.append(minute).append(" to ").append(nextHour);
+//        }
+    }
+
+    private int getNextHour(int hour) {
+        hour++;
+        if (hour > 23) {
+            return 0;
+        }
+        return hour;
+    }
+
+    private int get12hFormat(int hour24hFormat) {
+        if (hour24hFormat == 0) {
+            return 12;
+        } else if (hour24hFormat > 12) {
+            return hour24hFormat - 12;
+        }
+
+        return hour24hFormat;
+    }
+
+    private String getSuffix(int hour24hFormat) {
+        return (hour24hFormat < 12) ? "am" : "pm";
+    }
+}
